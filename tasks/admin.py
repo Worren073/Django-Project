0a5d2fab3d34858329a58e7486dfde50da60f35c
Worren073 <worrenalexanderbz@gmail.com>
2025-02-14 13:54:34 -0400

    Reparacion de error github y recuperacion de codigos corrompidos

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Task
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("date_creation", )
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
